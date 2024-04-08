39e8a19b75f19b368c428b2da894444531d9372f
Lucas Simpson <lucas.simpson0213@gmail.com>
2024-04-08 17:46:16 -0500

    creating test stub for testProductDelete

--- testing/ProductDeleteTest.php
+++ testing/ProductDeleteTest.php
@@ -1,5 +1,13 @@
-class ProductDataStub
+<?php
+include("")
+use PHPUnit\Framework\TestCase;
+    class ProductDeleteTest extends TestCase {
+        public function testProductDelete($productCode) {
+            $dbStub = $this->createStub(PDO::class);
+            $dbStub->prepare("")
 
+        }
+    }
 
 
 //preferably use stubs because mocks are more advanced and verify behavior with dependancy in the reaal worl.
